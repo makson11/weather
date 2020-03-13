0fa309217d8e9f4f2273de262a604e537911f4d0
makson11 <mamelnikov94@gmail.com>
2020-03-13 12:56:02 +1000

    Добавил анимации и окно с погодой

--- js/script.js
+++ js/script.js
@@ -1,5 +1,7 @@
-let search = document.querySelector('.weather-main_search'),
-    btn = document.querySelector('.weather-main_btn');
+let main = document.querySelector('.weather-main'),
+    search = document.querySelector('.weather-main_search'),
+    btn = document.querySelector('.weather-main_btn'),
+    info = document.querySelector('.weather-main_info');
 
 function runAnimation(event, style){
     search.addEventListener(event, () => {
@@ -9,3 +11,8 @@ function runAnimation(event, style){
 
 runAnimation('mouseover', 'none');
 runAnimation('mouseout', 'radial-pulse 1s infinite');
+
+btn.addEventListener('click', () =>{
+    main.style.transform = 'translate(-2500%, -50%)';
+    info.style.transform = 'translateY(0)';
+});
